9d0033e3d30094b310542d611b50804ee12ac1e6
phatpham9 <phatpham9@gmail.com>
2018-04-01 18:39:55 +0700

    fix(close #14): getting data of the current date not working

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fetch = require('isomorphic-fetch');
 
-const getHistorialUrl = date => `https://scraper.fun/scrape?s-url=https%3A%2F%2Fcoinmarketcap.com%2Fhistorical%2F${date.replace(/-/g, '')}%2F&s-scope=table%23currencies-all%20tbody%20tr&name=.currency-name%20.currency-name-container&symbol=.col-symbol&price=.price&marketCap=.market-cap&s-limit=100`;
+const getTodayDateString = () => {
+  const [dateString] = new Date().toISOString().match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/);
+
+  return dateString;
+};
+
+const getHistoricalUrl = date => `https://scraper.fun/scrape?s-url=https%3A%2F%2Fcoinmarketcap.com%2Fhistorical%2F${date.replace(/-/g, '')}%2F&s-scope=table%23currencies-all%20tbody%20tr&name=.currency-name%20.currency-name-container&symbol=.col-symbol&price=.price&marketCap=.market-cap&s-limit=100`;
 
 const presentUrl = 'https://scraper.fun/scrape?s-url=https%3A%2F%2Fcoinmarketcap.com&s-scope=table%23currencies%20tbody%20tr&s-limit=100&name=.currency-name%20.currency-name-container&symbol=.currency-name%20.currency-symbol&price=.price&marketCap=.market-cap';
 
@@ -21,7 +27,9 @@ const formatCoins = data => data.map(({ name, symbol, price, marketCap }, index)
 });
 
 const fetchCoins = async (date) => {
-  const res = await fetch(date ? getHistorialUrl(date) : presentUrl);
+  const res = await fetch(
+    date && date !== getTodayDateString() ? getHistoricalUrl(date) : presentUrl,
+  );
   const data = await res.json();
 
   return formatCoins(data);
@@ -75,7 +83,7 @@ const calculateROI = (coins, investmentOfEach) => {
   };
 };
 
-const calculate = async ({ from = '2017-01-01', to = '2018-01-07', min = 0, max = 0, ignores = [], top = 10, investmentOfEach = 1000 }) => {
+const calculate = async ({ top = 10, from = '2017-01-01', to = '2017-12-31', min = 0, max = 0, ignores = [], investmentOfEach = 1000 }) => {
   const coinsOnStartDate = await fetchCoins(from);
   const coinsOnEndDate = await fetchCoins(to);
 
